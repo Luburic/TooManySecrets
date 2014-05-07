0c914a92a5dd3c219da387d1170ccdeb64383351
Luburic <luburic91@gmail.com>
2014-05-07 10:59:54 +0200

    Inicijalni projekat za poslovnu

---
+++ Magacinsko Poslovanje/src/root/Application.java
@@ -0,0 +1,12 @@
+package root;
+
+import root.gui.MainFrame;
+
+public class Application {
+
+	public static void main(String[] args) {
+		MainFrame mf = new MainFrame();
+		mf.setVisible(true);
+	}
+
+}
---
+++ Magacinsko Poslovanje/src/root/gui/MainFrame.java
@@ -0,0 +1,12 @@
+package root.gui;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	private static final long serialVersionUID = 3706293806794500307L;
+
+	public MainFrame() {
+		this.setSize(500, 500);
+		this.setLocationRelativeTo(null);
+	}
+}
