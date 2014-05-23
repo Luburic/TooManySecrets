5e79d7b4ac0492ed07c7ae3c89cb245f89634a26
nex036ara <banexara@gmail.com>
2014-05-23 14:27:29 +0200

    table models

--- WsImplement/src/firma/gui/dialogs/FakturaDialog.java
+++ WsImplement/src/firma/gui/dialogs/FakturaDialog.java
@@ -3,14 +3,87 @@ package firma.gui.dialogs;
 import java.awt.Dialog;
 import java.awt.Dimension;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 import firma.gui.MainFrame;
-
 
 @SuppressWarnings("serial")
 public class FakturaDialog extends JDialog{
 
+	private JLabel lbNazivDob = new JLabel("Naziv dobavljaca:");
+	private JLabel lbAdrDob = new JLabel("Adresa dobavljaca:");
+	
+	private JLabel lbPibDob = new JLabel("Pib dobavljaca:");
+	private JLabel lbNazKup = new JLabel("Naziv kupca:");
+	
+	
+	
+	private JLabel lbAdrKup = new JLabel("Adresa kupca:");
+	private JLabel lbPibKup = new JLabel("Pib kupca:");
+	
+	private JLabel lbBrRac = new JLabel("Broj racuna:");
+	private JLabel lbDatRac = new JLabel("Datum racuna:");
+	
+	private JLabel lbVrRob = new JLabel("Vrednost robe:");
+	private JLabel lbVrUsl = new JLabel("Vrednost usluga:");
+	
+	private JLabel lbUkupRU = new JLabel("Ukupno roba/usluge:");
+	private JLabel lbUkupRab = new JLabel("Ukupan rabat:");
+	
+	private JLabel lbUkuPor = new JLabel("Ukupan porez:");
+	private JLabel lbOznVal = new JLabel("Oznaka valute:");
+	
+	
+	private JLabel lbIznUpl = new JLabel("Iznos za uplatu:");
+	private JLabel lbUplRac = new JLabel("Uplata na racun:");
+	
+	private JLabel lbDatVal = new JLabel("Datum valute:");
+	private JLabel lbIdPor = new JLabel("Id poruke:");
+
+	/****************smaranjeeeeee**************************/
+	
+	
+	private JTextField tfNazivDob = new JTextField();
+	private JTextField tfAdrDob = new JTextField();
+	
+	private JTextField tfPibDob = new JTextField();
+	private JTextField tfNazKup = new JTextField();
+	
+	private JTextField tfAdrKup = new JTextField();
+	private JTextField tfPibKup = new JTextField();
+	
+	
+	private JTextField tfBrRac = new JTextField();
+	private JTextField tfDatRac = new JTextField();
+	
+	
+	
+	private JTextField tfVrRob = new JTextField();
+	private JTextField tfVrUsl = new JTextField();
+	
+	private JTextField tfUkupRU = new JTextField();
+	private JTextField tfUkupRab = new JTextField();
+	
+	private JTextField tfUkuPor = new JTextField();
+	private JTextField tfOznVal = new JTextField();
+	
+	private JTextField tfIznUpl = new JTextField();
+	private JTextField tfUplRac = new JTextField();
+	
+	private JTextField tfDatVal = new JTextField();
+	private JTextField tfIdPor = new JTextField();
+	
+	/****************smaranjeeeeee**************************/
+	private JButton btnOk = new JButton("OK");
+	private JButton btnCancel = new JButton("Cancel");
+
+	//private CertificateGenerator cert = new CertificateGenerator();
+	
+	
+	
 	public FakturaDialog(MainFrame instance) {
 		super(instance, Dialog.ModalityType.APPLICATION_MODAL);
 		setTitle("Slanje fakture");		
@@ -22,6 +95,8 @@ public class FakturaDialog extends JDialog{
 				"20[]20[]20[]10[]10[]10[]10[]10[]20[]20");
 		setLayout(layout);*/
 		setLocationRelativeTo(instance);
+		
+		//forma za popunjavanje i slanje fakture
 	}
 	
 }
---
+++ WsImplement/src/firma/gui/tables/StavkeTableModel.java
@@ -0,0 +1,95 @@
+package firma.gui.tables;
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+import beans.faktura.Faktura.Stavka;
+
+@SuppressWarnings("serial")
+public class StavkeTableModel extends AbstractTableModel{
+
+	static final public String[] colHeaders = new String []{
+		"Redni broj",
+		"Roba/Usluge", 
+		"kolicina",
+		"Jedinica mere",
+		"Jedinicna cena",
+		"Vrednost",
+		"Procenat rabata",
+		"Iznos rabata",
+		"Umanjeno(rabat)",
+		"Ukupan porez",
+		};
+	
+	private ArrayList<Stavka> stavke;
+	
+	public StavkeTableModel() {
+		stavke = new ArrayList<Stavka>();
+	}
+	
+
+	public void fillTable(ArrayList<Stavka> stavke){
+		stavke.clear();
+		
+		for(Stavka s:stavke) {
+			Stavka ns = new Stavka();
+			
+			ns.setIznosRabata(s.getIznosRabata());
+			ns.setJedinicaMere(s.getJedinicaMere());
+			ns.setJedinicnaCena(s.getJedinicnaCena());
+			ns.setKolicina(s.getKolicina());
+			ns.setNazivRobeIliUsluge(s.getNazivRobeIliUsluge());
+			ns.setProcenatRabata(s.getProcenatRabata());
+			ns.setRedniBroj(s.getRedniBroj());
+			ns.setUkupanPorez(s.getUkupanPorez());
+			ns.setUmanjenoZaRabat(s.getUmanjenoZaRabat());
+			ns.setVrednost(s.getVrednost());
+			
+			stavke.add(ns);
+		}
+	}
+	
+	
+	@Override
+	public int getRowCount() {
+		return stavke.size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		return colHeaders.length;
+	}
+
+	public String getColumnName(int i) {
+		return colHeaders[i];
+	}
+	
+	
+	
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		
+		if( rowIndex<0 || rowIndex >= getRowCount())
+			return "";
+		
+		Stavka stavka = stavke.get(rowIndex);
+		
+		switch(columnIndex){
+		case 0:	return stavka.getRedniBroj();
+		case 1: return stavka.getNazivRobeIliUsluge();
+		case 2:	return stavka.getKolicina();
+		case 3: return stavka.getJedinicaMere();
+		case 4:	return stavka.getJedinicnaCena();
+		case 5: return stavka.getVrednost();
+		case 6:	return stavka.getProcenatRabata();
+		case 7: return stavka.getIznosRabata();
+		case 8:	return stavka.getUmanjenoZaRabat();
+		case 9: return stavka.getUkupanPorez();
+		
+
+		}
+		return "";	
+	}
+	
+}
